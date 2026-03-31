727adbb8b4648474e1b5001c2fc13cd6c111e291
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-03-31 11:05:19 -0300

    commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import { StyledHero } from "./HeroStyles";
 export default function Hero() {
     return (
         <StyledHero>
-            <h1>Interfaces limpas, soluções inteligentes.</h1>
+            <h1>Interfaces limpas,<br/>soluções inteligentes.</h1>
         </StyledHero>
     )
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,11 @@
 import Header from '../components/Header/Header'
+import Hero from '../components/Hero/Hero'
 
 export default function LandingPage() {
     return (
-        <Header/>
+        <>
+            <Header/>
+            <Hero/>
+        </>
     )
 }
