172a646b4ea379ce5ade5f7c152481e7b232d348
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-03-31 08:24:48 -0300

    hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,9 @@
+import { StyledHero } from "./HeroStyles";
+
+export default function Hero() {
+    return (
+        <StyledHero>
+            <h1>Interfaces limpas, soluções inteligentes.</h1>
+        </StyledHero>
+    )
+}
---
+++ src/components/Hero/HeroStyles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledHero = styled.hero`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+    height: 485px;
+`
