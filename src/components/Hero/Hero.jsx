6e9aa602769fa26b041a01321a1ed11d535c3fd9
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-04-28 11:03:41 -0300

    refactor: compressing components

--- src/components/Hero/Hero.jsx
+++
@@ -1,9 +0,0 @@
-import { StyledHero } from "./HeroStyles";
-
-export default function Hero() {
-    return (
-        <StyledHero>
-            <h1>Interfaces limpas,<br/>soluções inteligentes.</h1>
-        </StyledHero>
-    )
-}
